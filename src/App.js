23ae962225c519dd874ef51e5f22d09661d6a1da
Victor Oz <victorozurumba12@gmail.com>
2023-01-21 23:52:30 +0100

    added eslint

---
+++ src/App.js
@@ -0,0 +1,36 @@
+/** @format */
+
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ]);
+};
+
+const App = () => {
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      animal: "dog",
+      name: "Luna",
+      breed: "havanese",
+    }),
+    React.createElement(Pet, {
+      animal: "Bird",
+      name: "Pepper",
+      breed: "cockatiel",
+    }),
+    React.createElement(Pet, {
+      animal: "Cat",
+      name: "Doink",
+      breed: "Mixed",
+    })
+  );
+};
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App, {}, null));
