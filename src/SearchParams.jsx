bf35f598cd4597a042564fbeef8da78d2c7a51bf
Victor Oz <victorozurumba12@gmail.com>
2023-01-22 21:49:21 +0100

    added hook

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,16 +1,27 @@
+import { useState } from "react";
+let counter = 0
 const SearchParams = () => {
-    const location = "Seattle, WA";
-    return (
-      <div className="search-params">
-        <form>
-          <label htmlFor="location">
-            Location
-            <input id="location" value={location} placeholder="Location" />
-          </label>
-          <button>Submit</button>
-        </form>
-      </div>
-    );
-  };
-  
-  export default SearchParams;
+    counter++
+  const [location, updateLocation] = useState("");
+  return (
+    <div className="search-params">
+
+<h2>{counter}</h2>
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(e) => updateLocation(e.target.value)}
+          />
+          ;
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
