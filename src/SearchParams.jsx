dea33870fb4102508bd629ed7bc2a8cd7a137b75
Victor Oz <victorozurumba12@gmail.com>
2023-01-22 22:10:20 +0100

    added animal breeds

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
-let counter = 0
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
 const SearchParams = () => {
-    counter++
   const [location, updateLocation] = useState("");
+  const [animal, updateAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const breeds = ["poodle"];
+
   return (
     <div className="search-params">
-
-<h2>{counter}</h2>
       <form>
         <label htmlFor="location">
           Location
@@ -17,6 +19,45 @@ const SearchParams = () => {
             onChange={(e) => updateLocation(e.target.value)}
           />
           ;
+        </label>
+        <label htmlFor="animal">
+  Animal
+  <select
+    id="animal"
+    value={animal}
+    onChange={(e) => {
+      updateAnimal(e.target.value);
+      setBreed("");
+    }}
+    onBlur={(e) => {
+      updateAnimal(e.target.value);
+      setBreed("");
+    }}
+  >
+    <option />
+    {ANIMALS.map((animal) => (
+      <option key={animal} value={animal}>
+        {animal}
+      </option>
+    ))}
+  </select>
+</label>;
+<label htmlFor="breed">
+          Breed
+          <select
+            disabled={!breeds.length}
+            id="breed"
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+            onBlur={(e) => setBreed(e.target.value)}
+          >
+            <option />
+            {breeds.map((breed) => (
+              <option key={breed} value={breed}>
+                {breed}
+              </option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
