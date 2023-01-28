d094d5d15d788abae8898c68ee649a04c8e92f1c
Victor Oz <victorozurumba12@gmail.com>
2023-01-29 00:24:40 +0100

    added fommating chages to entre code base

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
 
-
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -17,14 +16,15 @@ const queryClient = new QueryClient({
 const App = () => {
   return (
     <BrowserRouter>
-    <QueryClientProvider client={queryClient}>
-<header>
-  <Link to="/">Adopt Me!</Link>
-</header>;
-      <Routes>
-        <Route path="/details/:id" element={<Details />} />
-        <Route path="/" element={<SearchParams />} />
-      </Routes>
+      <QueryClientProvider client={queryClient}>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        ;
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
       </QueryClientProvider>
     </BrowserRouter>
   );
